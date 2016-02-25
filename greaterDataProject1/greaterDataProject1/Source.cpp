3861495085a8cc23a1486cb768eaee5fb4b07674
ChancellorBill-Aventine <bmunshaw@outlook.com>
2016-02-25 00:09:30 -0600

    General improvements

--- greaterDataProject1/greaterDataProject1/Source.cpp
+++ greaterDataProject1/greaterDataProject1/Source.cpp
@@ -30,17 +30,17 @@ int main()
 	{
 		cout << "Select option: " << endl;
 		cout << "(1) Display assignments" << endl;
-		cout << "(2) Add assignment" << endl;
+		cout << "(2) Add assignment(s)" << endl;
 		cout << "(3) Edit an assignment" << endl;
 		cout << "(4) Complete assignment" << endl;
-		cout << "(5) Number of late assignments" << endl;
+		cout << "(5) Display number of late assignments" << endl;
 		cout << "(6) Save" << endl;
 		cout << "(7) Re-declare today's date" << endl;
 		cout << "(8) Exit" << endl;
 		cin >> userChoice;
 		switch (userChoice) 
 		{
-			case 1:
+			case 1: //Display assignments
 				cout << "Would you like to see assigned assignments (1) or completed assignments (2)?" << endl;
 				cin >> assignmentDisplaySpecify;
 				if (assignmentDisplaySpecify == 1 || assignmentDisplaySpecify == 2)
@@ -52,7 +52,7 @@ int main()
 					cout << "Incorrect option. Try again" << endl;
 				}
 				break;
-			case 2:
+			case 2: //Add assignments(s)
 				cout << "Would you like to add an assignment by typing it in (1), or bulk add assignments from the input file (2)" << endl;
 				cin >> assignmentAddSpecify;
 				if (assignmentAddSpecify == 1)
@@ -68,23 +68,23 @@ int main()
 					cout << "Incorrect option. Try again" << endl;
 				}
 				break;
-			case 3:
+			case 3: //Edit an assignment
 				cout << "State the due date of the assignment you would like to edit:" << endl;
 				cin >> tempDate;
 				assignments.editAssignment(tempDate);
 				break;
-			case 4:
+			case 4: //Complete assignment
 				cout << "State the assigned date of the assignment:" << endl;
 				cin >> tempDate;
 				assignments.completeAssignment(tempDate, today);
 				break;
-			case 5:
+			case 5: //Number of late assignments
 				cout << "You have " << assignments.lateAssignmentCount() << " late assignments." << endl;
 				break;
-			case 6:
+			case 6: //Save
 
 				break;
-			case 7:
+			case 7: //Redeclare Today's date
 				cout << "What is today?" << endl;
 				cin >> today;
 				break;
